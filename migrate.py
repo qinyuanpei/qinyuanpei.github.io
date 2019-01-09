488cc7a81bb6eae89c3b7ed938498e036c3920d9
= <=>
2019-01-09 18:07:08 +0800

    完成Migrate.py

--- migrate.py
+++ migrate.py
@@ -66,12 +66,29 @@ def sync(root,ak,sk,account,bucket):
         fileKey = link.split('/')[-1]
         if('http://img.blog.csdn.net' in link):
             sync_csdn(link)
-        elif('http://7wy477.com1.z0.glb.clouddn.com' in link):
+        elif('clouddn.com' in link):
             sync_qiniu(ak,sk,account,bucket,fileKey)
 
 # 上传图片至新浪图床
 def upload(src_file):
-    return weibo.get_image(src_file,'1546821636','WEIBO85374216')
+    url = "http://picupload.service.weibo.com/interface/pic_upload.php"
+    fileExt = src_file.split('.')[-1]
+    if(fileExt == 'png'):
+        fileExt = 'jpg'
+    timestamp = str(int(time.time()))
+    mimes = {"gif":'image%2Fgif','jpg':'image%2Fjpeg','jpeg':'image%2Fjpeg'}
+    querystring = {"mime":mimes[fileExt],"data":"base64","url":"0","markpos":"1","logo":"","nick":"0","marks":"1","app":"miniblog","cb":"http://weibo.com/aj/static/upimgback.html?_wv=5","callback":"STK_ijax_" + timestamp}
+    headers = {
+        'Cookie': "SINAGLOBAL=3235885370648.688.1545903019873; Ugrow-G0=57484c7c1ded49566c905773d5d00f82; SSOLoginState=1546821636; wvr=6; YF-V5-G0=694581d81c495bd4b6d62b3ba4f9f1c8; _s_tentry=login.sina.com.cn; UOR=www.baidu.com,open.weibo.com,login.sina.com.cn; Apache=312401166555.0963.1546821645121; ULV=1546821645903:2:1:1:312401166555.0963.1546821645121:1545903020312; YF-Page-G0=b003c396c5ae9fa75e3272879cbfcef2; wb_view_log_1278609231=1024*7681; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF3kQ_AqPEbbcvPj8FQZhJ35JpX5KMhUgL.Fo2ES0nceh.Ee022dJLoI7yBqPLV9H8Ddntt; ALF=1578468436; SCF=AvJtbZ0c6MGjVObXjJuM6TQkpI7QK5YTlzD1BQOMXHOyLUP1I0BlH4RkujbUtRWVOGbHzsC1zfQRc4T3UMinnLw.; SUB=_2A25xMCCGDeRhGedM7FoX8CfOyD2IHXVSRBVOrDV8PUNbmtBeLUjxkW9NWD4ssUCLazbUJY1ZcBkBt9Is6OKtwcQ7; SUHB=0D-Elw7e6ExMhl",
+    }
+    files = {'pic1':open(src_file,'rb').read()}
+    response = requests.request("POST", url, headers=headers, params=querystring,files=files)
+    if(response.status_code == 200):
+        result = re.sub(r"<meta.*</script>", "", response.text, flags=re.S)
+        image_result = json.loads(result)
+        image_id = image_result.get('data').get('pics').get('pic_1').get('pid')
+        return 'https://ws1.sinaimg.cn/large/{0}.{1}'.format(image_id,fileExt)
+
 
 
 if __name__ == "__main__":
