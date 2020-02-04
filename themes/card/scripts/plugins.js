d7d796ccd84e4d1e44750f86a37af5202285154e
= <=>
2020-02-04 16:30:59 +0800

    启用jsdelivr

--- themes/card/scripts/plugins.js
+++ themes/card/scripts/plugins.js
@@ -5,7 +5,8 @@ hexo.extend.helper.register('theme_version', () => version)
 const source = (path, cache, ext) => {
     if (cache) {
         const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`
-        return hexo.theme.config.cdn ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`
+        const jsdelivrCDN = hexo.config.jsdelivr;
+        return jsdelivrCDN.enable ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh-user}/${jsdelivrCDN.gh-repo}@latest${minFile}` : `${minFile}?v=${version}`
     } else {
         return path + ext
     }
