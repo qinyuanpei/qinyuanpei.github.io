3503118d150191ff3028ab9925c5591a5d7bda32
= <=>
2020-02-24 16:24:32 +0800

    支持语言统计

---
+++ languages.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import re
+import json
+
+# 列举所有博客
+def listPosts(rootPath):
+    posts = []
+    children = os.listdir(rootPath)
+    for child in children:
+        filePath = os.path.join(rootPath,child)
+        if os.path.isfile(filePath):
+            posts.append(filePath)
+        else:
+            posts.extend(listPosts(filePath))
+    return posts
+
+# 分析语言使用情况
+def analyseLanguages(posts):
+    languages = {}
+    for post in posts:
+        fi = open(post,'rt',encoding='utf-8')
+        text = list(map(lambda x:x.strip().replace('\n','').replace('\t',''), fi.readlines()))
+        matches = list(filter(lambda x:x.startswith('```') and len(x) > 3, text))
+        if len(matches) > 0:
+            for match in matches:
+                language = match[3:]
+                if language in ['shell','json','csharp','lua','yaml','yml']:
+                    print(post)
+                if language in languages.keys():
+                    languages[language] = languages[language] + 1
+                else:
+                    languages[language] = 1
+    return languages
+
+posts = listPosts('.\source\_posts')
+languages = analyseLanguages(posts)
+with open('languages.json','wt',encoding='utf-8') as f:
+    f.write(json.dumps(languages))
+
+
