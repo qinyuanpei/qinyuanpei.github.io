0c9e26587030e47ffbc8145370410c8ab5b771c9
qinyuanpei <yuanpei@wangdou-hbean.com>
2021-10-20 16:05:35 +0800

    增加异常处理

--- languages.py
+++ languages.py
@@ -19,26 +19,29 @@ def listPosts(rootPath):
 def analyseLanguages(posts):
     languages = {}
     for post in posts:
-        fi = open(post,'rt',encoding='utf-8')
-        text = list(map(lambda x:x.strip().replace('\n','').replace('\t',''), fi.readlines()))
-        matches = list(filter(lambda x:x.startswith('```') and len(x) > 3, text))
-        if len(matches) > 0:
-            for match in matches:
-                language = match[3:]
-                if language.lower() == "csharp":
-                   language = 'C#'
-                if language.lower() == "javascript" or language.lower() == "js":
-                   language = 'JavaScript'
-                if language.lower() == "shell":
-                   language = 'Shell'
-                if language.lower() == "yaml" or language.lower() == "yml":
-                   language = language.upper()
-                if language in ['shell','json','csharp','lua','yaml','yml']:
-                    print(post)
-                if language in languages.keys():
-                    languages[language] = languages[language] + 1
-                else:
-                    languages[language] = 1
+        try:
+            fi = open(post,'rt',encoding='utf-8')
+            text = list(map(lambda x:x.strip().replace('\n','').replace('\t',''), fi.readlines()))
+            matches = list(filter(lambda x:x.startswith('```') and len(x) > 3, text))
+            if len(matches) > 0:
+                for match in matches:
+                    language = match[3:]
+                    if language.lower() == "csharp":
+                        language = 'C#'
+                    if language.lower() == "javascript" or language.lower() == "js":
+                        language = 'JavaScript'
+                    if language.lower() == "shell" or language.lower() == "bash":
+                        language = 'Shell'
+                    if language.lower() == "yaml" or language.lower() == "yml":
+                        language = language.upper()
+                    if language in ['shell','json','csharp','lua','yaml','yml','plain']:
+                        print(f'{post} maybe need a check for code blocks' )
+                    if language in languages.keys():
+                        languages[language] = languages[language] + 1
+                    else:
+                        languages[language] = 1
+        except Exception as ex:
+            print(f'{post} maybe need a check for encoding' )
 
     #排序后取前5种语言输出        
     languages = sorted(languages.items(), key=lambda d:d[1], reverse = True)
